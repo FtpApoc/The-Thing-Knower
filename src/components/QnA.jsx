d2b04cd586287f9bc670da163bbf6ad047490f6f
FtpApoc <andrew.hughes0@me.com>
2024-10-02 15:28:52 +0100

    single integer semi functional

--- src/components/QnA.jsx
+++ src/components/QnA.jsx
@@ -1,29 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+// import Math from 'math';
+
 
 const QnA = () => {
     const [correct,setCorrect] = useState(null);
-    
 
-    const submitAnswer = () => {
-        
-    
+    const a = Math.floor(Math.random() * 10)
+    const b = Math.floor(Math.random() * 10)
+    const ans = a + b
+
+    const submitAnswer = (e) => {
+        e.preventDefault()
         const uAnswer = document.getElementById("testInput")
         
-        if (uAnswer.value == "2"){
+        if (uAnswer.value == String(ans)){
             setCorrect(true)
+            console.log("We Got Here! 18")
         } else {
             setCorrect(false)
         }
     }
+
     
     return ( 
         <div className="QnA"> 
             <div className="question-div">
-                <h1>What is 1+1?</h1>
+                <h1>What is {a}+{b}?</h1>
             </div>
             <div className="answer-div">
                 <input id="testInput"/> 
-                <button onClick={submitAnswer}>
+                <button onClick={(e) => {submitAnswer(e)}}>
                         Submit Answer    
                 </button>   
                      
