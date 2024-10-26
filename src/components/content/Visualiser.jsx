df3e3d91599f07cd5ba7f8180a49611b2ceaf020
FtpApoc <85648487+FtpApoc@users.noreply.github.com>
2024-10-26 01:03:50 +0100

    resolution text and colour fuctional

--- src/components/content/Visualiser.jsx
+++ src/components/content/Visualiser.jsx
@@ -3,67 +3,69 @@ import { useState } from 'react';
 
 const QnA = ({answer,question,nextQuestion}) => {
     const [userAns,setUserAns] = useState("")
-    const [correct,setCorrect] = useState(null);
+    const [resolved,setResolved] = useState(null);
+    const [resolutionMessage, setResolutionMessage] = useState(null);
+    let correct = null
 
     const submitAnswer = (event) => {
         event.preventDefault()
         const uAnswer = document.getElementById("testInput")
         
         if (uAnswer.value == String(answer)){
-            setCorrect(true)
+            correct = true
             handleResolved() 
         } else if (uAnswer.value == "idk"){
-            setUserAns("");
             handleResolved()
-        } else{
-            setCorrect(false)
+        } else {
+            setResolved(false)
         }
         
     }
 
-    const handleCorrect = () => {        
-        console.log("Correct") //This Is Never Activating 
-    }
-
-    const handleIncorrect = () => {
-        console.log("Incorrect")
-    }
-
     const handleResolved = () => {
-        correct ? handleCorrect() : handleIncorrect()
+        setResolved(true)
+        setResolutionMessage(
+            correct 
+            ? <h2 className="resMes" style={{color:'green'}}>That Is Correct!</h2>
+            : <h2 className="resMes" style={{color:'red'}}>The Answer was {answer}</h2>
+        )
+        
 
         setTimeout(() => {
-            setCorrect(false)
+            setResolved(false)
             setUserAns("");
             nextQuestion();
-        },1200)
+        },12000)
     }
 
 
 
     
     return ( 
-        <div className="QnA"> 
-            <div className="question-div">
-                <h1>{question}</h1>
+        <div className="content">
+            <div className="QnA"> 
+                <div className="question-div">
+                    <h1>{question}</h1>
+                </div>
+                <div className="answer-div">
+
+                    <button onClick={handleResolved}>Show Answer</button>
+
+                    <form onSubmit={(e) => {submitAnswer(e)}}>
+                        
+
+                        <input 
+                        id="testInput"
+                        value={userAns} 
+                        onChange={(e) => {setUserAns(e.target.value)}}
+                        /> 
+
+                        <input type='submit' value={"Submit Answer"} />  
+                        
+                    </form>
+                </div>
             </div>
-            <div className="answer-div">
-                <form onSubmit={(e) => {submitAnswer(e)}}>
-                    <input 
-                    id="testInput"
-                    value={userAns} 
-                    onChange={(e) => {setUserAns(e.target.value)}}
-                    /> 
-
-                    <input type='submit' value={"Submit Answer"} />  
-                     
-                </form>
-
-                <button onClick={handleResolved}>Show Answer</button>
-
-            </div>
-
-            {correct && <h2>That Is Correct!</h2>}
+            {resolved && resolutionMessage}
         </div>
      );
 }
