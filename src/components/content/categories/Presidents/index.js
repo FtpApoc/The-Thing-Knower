10b4b59be55932e2a65bdb5905a8ffbd59935838
FtpApoc <85648487+FtpApoc@users.noreply.github.com>
2024-10-30 00:54:23 +0000

    utility functions added

--- src/components/content/categories/Presidents/index.js
+++ src/components/content/categories/Presidents/index.js
@@ -1,14 +1,16 @@
 import presidents from './presidents.json'
-import { getRandomNumber } from '../../../Utilities';
+import { getRandomNumber, Ordinalise, pickRandomFromList } from '../../../Utilities';
+import { joinPaths } from '@remix-run/router';
 
 const Presidents = () => {
-    const president = presidents[getRandomNumber(presidents.length)]
+    // const president = presidents[getRandomNumber(presidents.length)]
+    const president = pickRandomFromList(presidents)["choice"]
 
-    const question = (`Who was the ${president["number"]}th(?) President?`)
+    const question = (`Who was the ${Ordinalise(president["number"])} President of the USA?`)
 
     const answer = president["president"]
 
-    return { question, answer};
+    return {question, answer};
 }
  
 export default Presidents;
